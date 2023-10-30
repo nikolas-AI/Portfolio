ba5e1b1bb3083c045ecb615d43b1ed9f94f91cf0
Nikolas <subedikiran678@gmail.com>
2023-10-30 16:46:23 +0545

    type writer

--- script.js
+++ script.js
@@ -15,32 +15,9 @@
 // window.onload = typeWriter;
 
 
-const phrases = ["I'm a Student", "I'm a Developer"];
-
-let currentPhrase = 0;
-let isDeleting = false;
-let i = 0;
-
-function typeWriter() {
-    const text = phrases[currentPhrase];
-    const speed = isDeleting ? 50 : 100;
-
-    if (!isDeleting && i <= text.length) {
-        document.getElementById('typewriter-text').innerHTML = text.substring(0, i);
-        i++;
-        setTimeout(typeWriter, speed);
-    } else if (isDeleting && i >= 0) {
-        document.getElementById('typewriter-text').innerHTML = text.substring(0, i);
-        i--;
-        setTimeout(typeWriter, speed);
-    } else {
-        isDeleting = !isDeleting;
-        if (isDeleting) {
-            currentPhrase = (currentPhrase + 1) % phrases.length;
-            i = text.length; // Set i to the length of the current phrase for erasing
-        }
-        setTimeout(typeWriter, 500);
-    }
-}
-
-window.onload = typeWriter;
+var typed = new Typed(".auto-type", {
+    strings: ["a Student.", "a Developer.", "an AI Enthusiast."],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+})
